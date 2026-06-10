569695d54307cf5184fc7ea1dbe98008f60b4ab4
Vijayant Singh <vijayantsingh123@gmail.com>
2026-06-10 13:38:17 +0530

    Updated scheduler

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -1,4 +1,5 @@
 import asyncio
+
 from core.nats_client import NATSClient
 from core.models import Job
 from storage.redis_store import RedisStore
@@ -6,15 +7,23 @@ from storage.redis_store import RedisStore
 store = RedisStore()
 
 async def run_scheduler():
+
     nats = NATSClient()
+
     await nats.connect()
     await nats.setup_stream()
 
     while True:
+
         jobs = store.get_due_jobs()
 
         for job_str in jobs:
+
             job = Job.from_json(job_str)
+
+            job.status = "DISPATCHED"
+
+            store.update_job(job)
 
             print(f"Dispatching job: {job.id}")
 
