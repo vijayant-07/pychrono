3df51ddbf168e6eeb08f09722a02d8d50bc8aacf
Vijayant Singh <vijayantsingh123@gmail.com>
2026-06-10 13:38:48 +0530

    redis store updated

--- storage/redis_store.py
+++ storage/redis_store.py
@@ -2,18 +2,115 @@ import redis
 import json
 import time
 
-r = redis.Redis(host="localhost", port=6379, decode_responses=True)
+r = redis.Redis(
+    host="localhost",
+    port=6379,
+    decode_responses=True
+)
 
 class RedisStore:
 
     def add_job(self, job):
-        r.zadd("jobs", {json.dumps(job.__dict__): job.run_at})
+
+        r.zadd(
+            "jobs",
+            {json.dumps(job.__dict__): job.run_at}
+        )
+
+        r.hset(
+            "job_details",
+            job.id,
+            json.dumps(job.__dict__)
+        )
 
     def get_due_jobs(self):
+
         now = time.time()
-        jobs = r.zrangebyscore("jobs", 0, now)
+
+        jobs = r.zrangebyscore(
+            "jobs",
+            0,
+            now
+        )
 
         if jobs:
-            r.zremrangebyscore("jobs", 0, now)
+            r.zremrangebyscore(
+                "jobs",
+                0,
+                now
+            )
 
         return jobs
+
+    def get_all_jobs(self):
+        return r.hgetall("job_details")
+
+    def get_job(self, job_id):
+        return r.hget("job_details", job_id)
+
+    def update_job(self, job):
+
+        r.hset(
+            "job_details",
+            job.id,
+            json.dumps(job.__dict__)
+        )
+
+    def update_status(self, job_id, status):
+
+        job_json = r.hget(
+            "job_details",
+            job_id
+        )
+
+        if not job_json:
+            return
+
+        job = json.loads(job_json)
+
+        job["status"] = status
+
+        r.hset(
+            "job_details",
+            job_id,
+            json.dumps(job)
+        )
+
+    def get_jobs_by_status(self, status):
+
+        jobs = []
+
+        for _, job_json in self.get_all_jobs().items():
+
+            job = json.loads(job_json)
+
+            if job["status"] == status:
+                jobs.append(job)
+
+        return jobs
+    
+    def get_stats(self):
+
+        stats = {
+            "PENDING": 0,
+            "DISPATCHED": 0,
+            "RUNNING": 0,
+            "RETRYING": 0,
+            "COMPLETED": 0,
+            "FAILED": 0
+        }
+
+        jobs = self.get_all_jobs()
+
+        for _, job_json in jobs.items():
+
+            job = json.loads(job_json)
+
+            status = job.get(
+                "status",
+                "PENDING"
+            )
+
+            stats[status] += 1
+
+        return stats
